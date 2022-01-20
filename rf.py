c153d51c2dbe415d05996875f36b25e55e986e1a
Christoph Redl <credl86@gmail.com>
2022-01-20 16:38:01 +0100

    update

--- rf.py
+++ rf.py
@@ -2,6 +2,9 @@
 import os
 import sys
 import keyboard
+import random
+import threading
+import PySimpleGUI as sg
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
@@ -11,8 +14,14 @@ import tensorflow.keras.initializers as initializers
 # Needed for animation
 import matplotlib.pyplot as plt
 
+abort=False
+ax_idx=0
+ay_idx=1
+px_idx=2
+py_idx=3
 width=50
 height=50
+box_size=10
 
 def construct_q_network(state_dim: int, action_dim: int) -> keras.Model:
     """Construct the critic network with q-values per action as output"""
@@ -42,115 +51,94 @@ def mean_squared_error_loss(q_value: tf.Tensor, reward: tf.Tensor) -> tf.Tensor:
     return loss
 
 def get_next_state(state, action):
-    ax = state[0,0].numpy()
-    ay = state[0,1].numpy()
-    px = state[0,2].numpy()
-    py = state[0,3].numpy()
+    succ_state = np.array(state)
     if action == 0:
-        ax = max(0, ax - 1)
+        succ_state[ax_idx] = max(0, state[ax_idx] - 1)
     elif action == 1:
-        ax = min(width - 1, ax + 1)
+        succ_state[ax_idx] = min(width - 1, state[ax_idx] + 1)
     elif action == 2:
-        ay = max(0, ay - 1)
+        succ_state[ay_idx] = max(0, state[ay_idx] - 1)
     elif action == 3:
-        ay = min(height - 1, ay + 1)
-    ret = tf.constant([[ax,ay,px,py]])
-    return ret
+        succ_state[ay_idx] = min(height - 1, state[ay_idx] + 1)
+    return succ_state
+
+def action_name(action):
+    if action == 0:
+        return "left"
+    elif action == 1:
+        return "right"
+    elif action == 2:
+        return "up"
+    elif action == 3:
+        return "down"
+    return state
 
 def get_reward(state, action):
     succ_state = get_next_state(state, action)
-    ax = state[0,0].numpy()
-    ay = state[0,1].numpy()
-    px = state[0,2].numpy()
-    py = state[0,3].numpy()
     # try to stay at player's position
     #reward = [-abs(ax - px) - abs(ay - py)]
-    reward = [-abs(25 - ax) - abs(25 - ay)]
+    #reward = -succ_state[ax_idx] + succ_state[ay_idx]
+    reward = -abs(25 - succ_state[ax_idx]) - abs(25 - succ_state[ay_idx])
     return reward
 
 def format_state(state):
-    ax = state[0,0].numpy()
-    ay = state[0,1].numpy()
-    px = state[0,2].numpy()
-    py = state[0,3].numpy()
-    return "{}".format(ax) + "/" + "{}".format(ay) + ", " + "{}".format(px) + "/" + "{}".format(py)
+    return "{}".format(state[ax_idx]) + "/" + "{}".format(state[ay_idx]) + ", " + "{}".format(state[px_idx]) + "/" + "{}".format(state[py_idx])
 
 def format_step(state, action):
     succ_state = get_next_state(state, action)
     reward = get_reward(state, action)
     return "(" + format_state(state) + " -> " + format_state(succ_state) + ", A: " + "{}".format(action) + ", R:" + "{}".format(reward) + ")"
 
-def visualizefield(state):
-    ax = state[0,0].numpy()
-    ay = state[0,1].numpy()
-    px = state[0,2].numpy()
-    py = state[0,3].numpy()
-    over = 0
-    str = ""
-    for yc in range(0,height):
-        over = 0
-        for xc in range(0, width):
-            if xc == ax and yc == ay:
-                #statestr = format_state(state)
-                str = str + "x" #+ statestr
-                #over = len(statestr)
-            elif xc == px and yc == py:
-                #statestr = format_state(state)
-                str = str + "@" #+ statestr
-                #over = len(statestr)
-            else:
-                if over == 0:
-                    if yc == 0 or yc == height - 1 or xc == 0 or xc == width - 1:
-                        str = str + "#"
-                    elif xc == 0 and yc == 0:
-                        str = str + "."
-                    else:
-                        str = str + " "
-                else:
-                    over = over - 1
-        str = str + "\n"
-    #os.system('cls')
-    sys.stdout.write(str)
+def visualizefield(main_window, state):
+    #main_window["-CANV-"].DrawRectangle((0, 0), (width * box_size, height * box_size), fill_color="white")
+    main_window["-CANV-"].DrawRectangle((0 * box_size, 0 * box_size), (width * box_size - 1, 1 * box_size - 1), fill_color="black")
+    main_window["-CANV-"].DrawRectangle((0 * box_size, (height - 1) * box_size), (width * box_size - 1, height * box_size - 1), fill_color="black")
+    main_window["-CANV-"].DrawRectangle((0 * box_size, 0 * box_size), (1 * box_size - 1, height * box_size - 1), fill_color="black")
+    main_window["-CANV-"].DrawRectangle(((width - 1) * box_size, 0 * box_size), (width * box_size - 1, height * box_size - 1), fill_color="black")
+    main_window["-CANV-"].DrawRectangle((state[ax_idx] * box_size, state[ay_idx] * box_size), ((state[ax_idx] + 1) * box_size - 1, (state[ay_idx] + 1) * box_size - 1), fill_color="red")
+    main_window["-CANV-"].DrawRectangle((state[px_idx] * box_size, state[py_idx] * box_size), ((state[px_idx] + 1) * box_size - 1, (state[py_idx] + 1) * box_size - 1), fill_color="yellow")
     
 def update_state_by_user_input(state):
-    ax = state[0,0].numpy()
-    ay = state[0,1].numpy()
-    px = state[0,2].numpy()
-    py = state[0,3].numpy()
     if keyboard.is_pressed('a'):
-        px = max(px - 1, 0)
+        state[px_idx] = state[px_idx] = max(state[px_idx] - 1, 0)
     if keyboard.is_pressed('d'):
-        px = min(px + 1, width - 1)
+        state[px_idx] = min(state[px_idx] + 1, width - 1)
     if keyboard.is_pressed('w'):
-        py = max(py - 1, 0)
+        state[py_idx] = max(state[py_idx] - 1, 0)
     if keyboard.is_pressed('s'):
-        py = min(py + 1, height - 1)
-    return tf.constant([[ax,ay,px,py]])
- 
-if __name__ == "__main__":
+        state[py_idx] = min(state[py_idx] + 1, height - 1)
+    return state
 
+def game_loop(main_window):
     # hyperparameters
     action_dim = 4
     exploration_rate = 0.1
     learning_rate = 0.01
     num_episodes = 1000000
-    alpha = 0.1
-    gamma = 0.0
-    succ_state = tf.constant([[0,0,10,10]])
+    alpha = 0.10
+    gamma = 0.99
+    succ_state = [0, 0, 10, 10]
     state_dim = 4
+    print_interval = 1
+    max_sample_storage = 10000
+    training_interval = 1000
 
     # construct q-network
     q_network = construct_q_network(state_dim, action_dim)
     opt = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    q_network.compile(opt, loss="mse")
     
-    i = 0
+    trainingset = list()
+    step = 0
     while True:
-        i = i + 1
+        step = step + 1
+        if abort:
+            break
         with tf.GradientTape() as tape:
             # define current state and obtain q values (estimated by NN)
             state = succ_state
             state = update_state_by_user_input(state)
-            q_values = q_network(state)
+            q_values = q_network(tf.constant([state]))
 
             # choose action
             epsilon = np.random.rand()
@@ -161,21 +149,63 @@ if __name__ == "__main__":
 
             # go to successor state and obtain reward
             succ_state = get_next_state(state, action)
-            succ_state_q_values = q_network(succ_state)
+            succ_state_q_values = q_network(tf.constant([succ_state]))[0].numpy()
             reward = get_reward(state, action)
 
-            # visualize
-            if i % 100 == 0:
-                visualizefield(succ_state)
-                sys.stdout.write(format_step(state, action))
-                sys.stdout.write("\n")
-                sys.stdout.flush()
-
             # update q-value
-            q_value = q_values[0, action]
+            q_value = q_values[0, action].numpy()
             new_q_value = q_value + alpha * (reward + gamma * max(succ_state_q_values) - q_value)
+            succ_state_q_values[action] = new_q_value
+
+            # store observation in training set
+            trainingsample = (tuple(state), action, tuple(succ_state), tuple(succ_state_q_values))
+            #trainingsample = tf.constant(state, succ_state_q_values)
+            #trainingsample = tf.constant([state, succ_state_q_values])
+            #print("T", tensor)
+            if len(trainingset) >= max_sample_storage:
+                trainingset[random.randint(0, max_sample_storage - 1)] = trainingsample
+            else:
+                trainingset.append(trainingsample)
 
             # compute loss value and do NN learn
-            loss_value = mean_squared_error_loss(q_value, new_q_value)
-            grads = tape.gradient(loss_value[0], q_network.trainable_variables)
-            opt.apply_gradients(zip(grads, q_network.trainable_variables))
+            if step % training_interval == 0:
+                #loss_value = mean_squared_error_loss(q_value, new_q_value)
+                sample = random.sample(trainingset, min(len(trainingset), 10))
+                inp = tf.constant([ list(s) for (s, a, ss, q) in sample ])
+                out = tf.constant([ list(q) for (s, a, ss, q) in sample ])
+                #q_network.fit(inp, out)
+            
+            # visualize
+            if step % print_interval == 0:
+                visualizefield(main_window, succ_state)
+                #sys.stdout.write(format_step(state, action))
+                #sys.stdout.write("\n")
+                #print(trainingsample, "chosen action:", action_name(action), ", direct reward:", reward)
+                #sys.stdout.flush()
+                #main_window["-CANV-"].update()
+
+            #q_network.fit()
+            #grads = tape.gradient(loss_value[0], q_network.trainable_variables)
+            #opt.apply_gradients(zip(grads, q_network.trainable_variables))
+
+if __name__ == "__main__":
+    abort = False
+    main_window = sg.Window(title="RL",layout = [
+                                                [
+                                                    sg.Graph(
+                                                        canvas_size=(width * box_size, height * box_size),
+                                                        graph_bottom_left=(0, height * box_size),
+                                                        graph_top_right=(width * box_size, 0),
+                                                        key="-CANV-",
+                                                        pad=(0,0)
+                                                    )
+                                                ]
+                                            ], size=(width * box_size,height * box_size), margins=(0, 0), finalize=True)
+    gl = threading.Thread(target=game_loop, args=(main_window,))
+    gl.start()
+    while True:
+        event, values = main_window.read()
+        if event == sg.WIN_CLOSED:
+            abort = True
+            break
+    gl.join()
