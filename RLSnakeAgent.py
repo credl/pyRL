6628c2620c4c07c2c3faafff1b234048d7fcbfb5
Christoph Redl <credl86@gmail.com>
2023-02-27 01:43:28 +0100

    improve snake agent

--- RLSnakeAgent.py
+++ RLSnakeAgent.py
@@ -10,7 +10,7 @@ class SnakeEnvironment(RLFramework.RLEnvironment):
     inpkey = 0
     AC_LEFT: int = 0; AC_RIGHT: int = 1; AC_UP: int = 2; AC_DOWN: int = 3; AC_NOTHING = 4
     direction = AC_LEFT
-    WIDTH: int = 25; HEIGHT: int = 25
+    WIDTH: int = 10; HEIGHT: int = 10
     COLL_RADIUS = 2
     agent_x: int = 0; agent_y: int = 0; coin_x = -1; coin_y = -1
     snakelen: int = 2
@@ -23,11 +23,13 @@ class SnakeEnvironment(RLFramework.RLEnvironment):
     survived_len = []
     survived_steps = []
     step = 0
+    walls = []
 
     def __init__(self):
         # set initial state
         self.agent_x = int(self.WIDTH / 2)
         self.agent_y = int(self.HEIGHT / 2)
+        self.walls = [ (x, 0) for x in range(self.WIDTH) ] + [ (x, self.HEIGHT - 1) for x in range(self.WIDTH) ] + [ (0, y) for y in range(self.HEIGHT) ] + [ (self.WIDTH - 1, y) for y in range(self.HEIGHT) ]
 
     def get_state_dim(self):
         return len(self.__encode_state())
@@ -57,34 +59,34 @@ class SnakeEnvironment(RLFramework.RLEnvironment):
         elif self.direction == self.AC_DOWN:    self.agent_y += 1
         # do not bump into walls or snake
         coll = False
-        if self.agent_x < 0 or self.agent_x >= self.WIDTH or self.agent_y < 0 or self.agent_y >= self.HEIGHT:
+        if (self.agent_x, self.agent_y) in self.walls:
             coll = True
         if (self.agent_x, self.agent_y) in self.snakeelem:
             coll = True
         if coll:
-            reward += -100
+            reward += self.snakelen * 10 -1000
             self.agent_x = -1
             self.agent_y = -1
             finished = True
         # compute reward for collecting coins
         if self.coin_x != -1 and self.coin_y != -1 and abs(self.agent_x - self.coin_x) < self.COLL_RADIUS and abs(self.agent_y - self.coin_y) < self.COLL_RADIUS:
             reward += 100
-            finished = True
+            finished = False
             self.coin_x = -1
             self.coin_y = -1
             self.snakelen += 1
         self.overall_rewards += [reward]
         if self.agent_x == -1 and self.agent_y == -1:
-            self.agent_x = np.random.choice(self.WIDTH)
-            self.agent_y = np.random.choice(self.HEIGHT)
+            self.agent_x = 1 + np.random.choice(self.WIDTH - 2)
+            self.agent_y = 1 + np.random.choice(self.HEIGHT - 2)
             self.snakeelem = []
             self.survived_len += [self.snakelen]
             self.survived_steps += [self.step]
             self.snakelen = 2
             self.step = 0
         if self.coin_x == -1 and self.coin_y == -1:
-            self.coin_x = np.random.choice(self.WIDTH)
-            self.coin_y = np.random.choice(self.HEIGHT)
+            self.coin_x = 1 + np.random.choice(self.WIDTH - 2)
+            self.coin_y = 1 + np.random.choice(self.HEIGHT - 2)
         return (self.get_state(), reward, finished)
 
     def get_state(self):
@@ -92,8 +94,9 @@ class SnakeEnvironment(RLFramework.RLEnvironment):
 
     def visualize(self, rlframework, step):
         out = [ [" "] * self.WIDTH for i in range(self.HEIGHT)]
+        for (x, y) in self.walls:
+            out[x][y] = "+"
         # print agent and player
-        out[0][0] = out[0][self.WIDTH - 1] = out[self.HEIGHT - 1][0] = out[self.HEIGHT - 1][self.WIDTH - 1] = "+"
         # print agent and player
         out[self.agent_y][self.agent_x] = "X"
         if self.coin_x != -1 and self.coin_y != -1:
@@ -143,6 +146,8 @@ class SnakeEnvironment(RLFramework.RLEnvironment):
         state = [ [0.0, 0.0, 0.0] for j in range(self.HEIGHT * self.WIDTH)]
         state[self.__coord_to_idx(self.agent_x, self.agent_y)][0] = 1.0
         state[self.__coord_to_idx(self.coin_x, self.coin_y)][1] = 1.0
+        for (x, y) in self.walls:
+            state[self.__coord_to_idx(x,y)][2] = 1.0
         for (x,y) in self.snakeelem:
             state[self.__coord_to_idx(x,y)][2] = 1.0
         return state
@@ -152,6 +157,8 @@ class SnakeEnvironment(RLFramework.RLEnvironment):
         state = [ [ [0.0, 0.0, 0.0] for j in range(self.HEIGHT) ] for i in range(self.WIDTH)]
         state[self.agent_y][self.agent_x][0] = 1.0
         state[self.coin_y][self.coin_x][1] = 1.0
+        for (x, y) in self.walls:
+            state[x][y][2] = 1.0
         for (x,y) in self.snakeelem:
             state[y][x][2] = 1.0
         return state
@@ -164,10 +171,9 @@ if __name__ == "__main__":
     env = SnakeEnvironment()
     net = keras.models.Sequential([
                 keras.layers.Reshape((env.WIDTH, env.HEIGHT, 3), input_shape=(env.WIDTH, env.HEIGHT, 3)),
-                keras.layers.Conv2D(1, kernel_size=(2, 2), strides=(2, 2), padding='same', activation="leaky_relu"),
+                keras.layers.Conv2D(10, kernel_size=(2, 2), strides=(1, 1), padding='same', activation="leaky_relu"),
                 keras.layers.MaxPooling2D((1, 1), strides=1),
                 keras.layers.Flatten(),
-                keras.layers.Dense(20, activation="leaky_relu"),
                 keras.layers.Dense(20, activation="leaky_relu"),
                 keras.layers.Dense(env.get_action_dim(), activation="linear", kernel_initializer='random_normal', bias_initializer='random_normal')
 
@@ -176,7 +182,7 @@ if __name__ == "__main__":
 #                keras.layers.Dense(300, activation="leaky_relu"),
 #                keras.layers.Dense(env.get_action_dim())
             ])
-    tr = RLFramework.RLTrainer(env, nn=net, visualize_interval=1, load_path="./RLSnakeAgent_trained.h5", save_path="./RLSnakeAgent_trained.h5", exploration_rate_start=0.99, exploration_rate_decrease=0.001, save_interval=100, gamma_discout_factor=0.2, nn_learning_rate=0.03, replay_buffer_size=2000, sample_size=64, accept_q_network_interval=1)
+    tr = RLFramework.RLTrainer(env, nn=net, visualize_interval=1, load_path="./RLSnakeAgent_trained.h5", save_path="./RLSnakeAgent_trained.h5", exploration_rate_start=0.99, exploration_rate_decrease=0.001, save_interval=100, gamma_discout_factor=0.3, nn_learning_rate=0.05, replay_buffer_size=2000, sample_size=64, accept_q_network_interval=1)
     tr.get_action(env.get_state())
     print("Network stats:\n"  + tr.get_network_stats())
     cons.myprint("Network stats:\n" + tr.get_network_stats())
